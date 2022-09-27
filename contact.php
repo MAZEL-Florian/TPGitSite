11310b0a39a1f129759e1c47397f7658f730f595
MAZEL Florian <mazel.florian1@gmail.com>
2022-09-27 16:01:52 +0200

    MAJ du contact

--- contact.php
+++ contact.php
@@ -20,64 +20,66 @@
         <link rel="stylesheet" href="cours.css">
     </head>
     <body>
-        <h1 class="text-center">Contact</h1>  
+        <h1 class="text-center py-5">Contact</h1>  
         <div class="container">
-        <form class="row g-3">
-            <div class="col-md-4">
+        <form class="row g-3" method="post">
+            <div class="col-md-6">
                 <label for="validationServer01" class="form-label">Nom</label>
-                <input type="text" class="form-control" id="nom" required>
-                <div class="valid-feedback">
-                Looks good!
-                </div>
+                <input type="text" class="form-control" name="nom" required>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <label for="validationServer02" class="form-label">Prénom</label>
-                <input type="text" class="form-control" id="prenom" required>
-                <div class="valid-feedback">
-                Looks good!
-                </div>
+                <input type="text" class="form-control" name="prenom" required>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <label for="validationServerUsername" class="form-label">Nom de société</label>
-                <input type="text" class="form-control" id="nomsociete" required>
-                <div id="validationServerUsernameFeedback" class="invalid-feedback">
-                    Please choose a username.
-                </div>
+                <input type="text" class="form-control" name="nomsociete" required>
             </div>
             <div class="col-md-6">
                 <label for="validationServer03" class="form-label">Ville</label>
-                <input type="text" class="form-control" id="ville" required>
-                <div id="validationServer03Feedback" class="invalid-feedback">
-                Please provide a valid city.
-                </div>
+                <input type="text" class="form-control" name="ville" required>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="validationServer04" class="form-label">Rue</label>
-                <input type="text" class="form-control" id="rue" required>
-                <div id="validationServer03Feedback" class="invalid-feedback">
-                Please provide a valid city.
-                </div>
+                <input type="text" class="form-control" name="rue" required>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="validationServer05" class="form-label">Code Postal</label>
-                <input type="text" class="form-control" id="cp" required>
+                <input type="text" class="form-control" name="cp" required>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="validationServer05" class="form-label">Pays</label>
-                <input type="text" class="form-control" id="pays" required>
+                <input type="text" class="form-control" name="pays" required>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="validationServer05" class="form-label">Téléphone</label>
-                <input type="text" class="form-control" id="tel" required>
+                <input type="text" class="form-control" name="tel" required>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="validationServer05" class="form-label">Adresse mail</label>
-                <input type="email" class="form-control" id="mail" required>
+                <input type="email" class="form-control" name="mail" required>
             </div>
-            <div class="col-12">
+            <div class="text-center py-4">
                 <button class="btn btn-primary" type="submit">Ajouter</button>
             </div>
         </form>
-</div>
+        <?php
+                if(isset($_POST['submit'])) 
+        {
+            echo("First name: " . $_POST['nom'] . "<br />\\n");
+            echo("Last name: " . $_POST['prenom'] . "<br />\\n");
+            echo("Last name: " . $_POST['nomsociete'] . "<br />\\n");
+            echo("Last name: " . $_POST['ville'] . "<br />\\n");
+            echo("Last name: " . $_POST['rue'] . "<br />\\n");
+            echo("Last name: " . $_POST['cp'] . "<br />\\n");
+            echo("Last name: " . $_POST['pays'] . "<br />\\n");
+            echo("Last name: " . $_POST['tel'] . "<br />\\n");
+            echo("Last name: " . $_POST['mail'] . "<br />\\n");
+        }
+        else {
+            echo "marche pas";
+        }
+        ?>
+        </div>
     </body>
 </html>
